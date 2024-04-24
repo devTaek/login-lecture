ca70a17ffd4592bff156f3e6f48d2dd45f388c24
d-taegyun <d.taegyun@gmail.com>
2024-04-25 03:12:40 +0900

    UserStorage 모델 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,30 @@
+"use strict";
+
+class UserStorage {
+  // class 안에 변수를 선언할 때는 선언자(const)가 필요없다.
+  // 다른 곳에서 UserStorage의 이 class 자체의 users에 접근할 때는 static 으로 선언해주어 정적변수로 만들어줘야한다.
+
+  // 원래는 다이렉트로 접근할 수 없어야해요. (외부에서 데이터를 가져올수 없도록)
+  // 그래서 이 변수를 은닉화 시켜줘야한다. (#변수)
+  // public한 변수에서 private한 변수로 바꿔줘야한다.
+  static #users = {
+    id: ["harold", "harold3", "harold109"],
+    psword: ["123", "1234", "12345"],
+    name: ["정태균", "태균", "택"],
+  };
+
+  // class 자체에서 메서드에 접근하려면 static을 붙여줘야한다.
+  static getUsers(...fields) {
+    // 은닉화된 users를 반환
+    const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if(users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
+}
+
+module.exports = UserStorage;
